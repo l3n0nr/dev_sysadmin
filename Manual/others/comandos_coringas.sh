2bbe1e6dd7cd63e3002f26cc3aa24d868e8f88c8
lenonr <lenonrmsouza@gmail.com>
2017-11-07 09:00:32 -0200

    comandos coringas com pipe

---
+++ Manual/others/comandos_coringas.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+# retornando sequencia de array
+# seq 100         # lista 100 numeros
+
+# comando para embaralhar valores
+# shuf "lista.txt"
+
+# exemplo
+# seq 100 | shuf  # vai mostrar 100 valores embaralhados
+
+# restrindo numero maximo de valores mostrados
+# seq 100 | shuf | head -n1
