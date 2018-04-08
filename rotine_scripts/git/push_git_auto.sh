6e89cfbbe8ebb3df555a5509b7402754d02ab8ab
lenonr <lenonrmsouza@gmail.com>
2018-04-08 19:38:02 -0300

    script otimizacao git adicionado - status

---
+++ rotine_scripts/git/push_git_auto.sh
@@ -0,0 +1,84 @@
+#!/bin/zsh
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# # # # # # # # # # # #
+#   DEVELOPMENT BY 	  #
+# # # # # # # # # # # #
+#
+# por lenonr(Lenon Ricardo)
+#       contato: <lenonrmsouza@gmail.com>
+#
+# # # # # # # # # # # # # # # # # # # # # # # ## # # #
+# Date create script:    	  		[08/04/18]       #
+# Last modification script: 		[08/04/18]       #
+# # # # # # # # # # # # # # # # # # # # # # # ## # # #
+#
+# # variaveis
+# pasta dos repositorios
+LOCAL='/home/lenonr/Github/'		# pasta do repositorio
+
+# repositorios disponiveis
+repos=(dev_xfce dev_scripts dev_ksp dev_sysadmin dev_web dev_clonerepo)		# repositorios
+
+auto_git()
+{
+	clear
+
+	# # walk to the array
+	for (( i = 1; i <= ${#repos[@]}; i++ )); do	
+		# verify local repo disk
+		if [[ $LOCAL${repos[$i]} != $LOCAL ]]; then
+			# verify local repo
+			if [ -e "$LOCAL${repos[$i]}" ]; then 	  	 
+				# if update repositorie work
+			  	if [[ $? == "0" ]]; then
+		  			# into folder location
+				  	cd $LOCAL${repos[$i]}			  					
+
+					# check status
+					git status | grep "Changes not" > /dev/null
+						
+					# if value = 0, then comparation is true
+					if [[ $? == "0" ]]; then
+						# show folder status
+						echo "\nRepositorio: $LOCAL${repos[$i]}\n"
+						git status
+					else
+						printf ""
+					fi				
+
+					git status | grep "Untracked files:" > /dev/null
+
+					# if value = 0, then comparation is true
+					if [[ $? == "0" ]]; then
+						#show folder status
+						echo "\nRepositorio: $LOCAL${repos[$i]}\n"
+						git status
+					else
+						printf ""
+					fi			
+					
+				# if update repositorie not work		
+			  	else				  		
+					echo "repositorie Error ${repos[$i]}!" >> /tmp/repo.txt
+			  	fi				
+				
+				# add  
+			  	let repo_founds++		
+
+			  	printf "\n" >> /tmp/repo.txt  	
+			else
+				date >> /tmp/repo.txt
+				echo "[-] - Not found": $LOCAL${repos[$i]}
+
+				# repo_notfounds=$(($repo_notfounds + 1));        
+				let repo_notfounds++
+			fi
+		fi
+	done		
+}
+
+auto_git
+
+# data do final do script
+date >> /tmp/repo.txt
