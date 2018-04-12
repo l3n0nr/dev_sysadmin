8c7ca50296416af691b3af9cd6e92a13ee09bc7f
lenonr <lenonrmsouza@gmail.com>
2018-04-12 12:49:08 -0300

    arquivo verificado e melhorado

--- rotine_scripts/arquivos_git/pull_git.sh
+++ rotine_scripts/arquivos_git/pull_git.sh
@@ -17,7 +17,7 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # ## # # #
 # Date create script:    	  		[30/03/18]       #
-# Last modification script: 		[05/04/18]       #
+# Last modification script: 		[12/04/18]       #
 # # # # # # # # # # # # # # # # # # # # # # # ## # # #
 
 # # variaveis
@@ -27,13 +27,9 @@ LOCAL='/home/lenonr/Github/'		# pasta do repositorio
 # repositorios disponiveis
 REPOS=(dev_xfce dev_scripts dev_ksp dev_sysadmin dev_web dev_clonerepo)		# repositorios
 
-# intervalo de tempo
-# TEMPO=14400s 						# sera executado 3 vezes por dia a cada 4 horas
-# TEMPO_DATE="14400 seconds"
-
 internet()
 {
-	ping -q -c1 google.com > /dev/null
+	ping -c1 google.com > /dev/null
 
 	if [ $? = "0" ]; then		
 		pull_git
@@ -45,63 +41,47 @@ internet()
 
 pull_git()
 {
-	# intervalo de tempo 
-	# while true; do	
-		#mostrando mensagem
-		# notify-send -u normal "Atualizando repositorios do Github" -t 6000				
+	# # walk to the array
+	for (( i = 1; i <= ${#REPOS[@]}; i++ )); do	
+		# verify local repo disk
+		if [[ $LOCAL${REPOS[$i]} != $LOCAL ]]; then
+			# verify local repo
+			if [ -e "$LOCAL${REPOS[$i]}" ]; then 	  	 
+				date >> /tmp/repo.txt
+			  	echo "[+] - Atualizando repositorio:" $LOCAL${REPOS[$i]} >> /tmp/repo.txt
 
-		# # walk to the array
-		for (( i = 1; i <= ${#REPOS[@]}; i++ )); do	
-			# verify local repo disk
-			if [[ $LOCAL${REPOS[$i]} != $LOCAL ]]; then
-				# verify local repo
-				if [ -e "$LOCAL${REPOS[$i]}" ]; then 	  	 
-					date >> /tmp/repo.txt
-				  	echo "[+] - Atualizando repositorio:" $LOCAL${REPOS[$i]} >> /tmp/repo.txt
+			  	# into folder location
+			  	cd $LOCAL${REPOS[$i]}
 
-				  	# into folder location
-				  	cd $LOCAL${REPOS[$i]}
+			  	# update repositories
+			  	git pull >> /tmp/repo.txt
 
-				  	# update repositories
-				  	git pull >> /tmp/repo.txt
+			  	# if update repositorie not work
+			  	if [[ $? == "0" ]]; then
+					# echo "Repositorie ${REPOS[$i]} fine!" >> /tmp/repo.txt
+			  	else				  		
+					echo "Repositorie Error ${REPOS[$i]}!" >> /tmp/repo.txt
+			  	fi
 
-				  	# if update repositorie not work
-				  	if [[ $? == "0" ]]; then
-						# echo "Repositorie ${REPOS[$i]} fine!" >> /tmp/repo.txt
-				  	else				  		
-						echo "Repositorie Error ${REPOS[$i]}!" >> /tmp/repo.txt
-				  	fi
+			  	# REPO_FOUNDS=$(($REPO_FOUNDS + 1));        
+			  	let REPO_FOUNDS++		
 
-				  	# REPO_FOUNDS=$(($REPO_FOUNDS + 1));        
-				  	let REPO_FOUNDS++		
+			  	printf "\n" >> /tmp/repo.txt  	
+			else
+				date >> /tmp/repo.txt
+				echo "[-] - Not found": $LOCAL${REPOS[$i]}
 
-				  	printf "\n" >> /tmp/repo.txt  	
-				else
-					date >> /tmp/repo.txt
-					echo "[-] - Not found": $LOCAL${REPOS[$i]}
-
-					# REPO_NOTFOUNDS=$(($REPO_NOTFOUNDS + 1));        
-					let REPO_NOTFOUNDS++
-				fi
+				# REPO_NOTFOUNDS=$(($REPO_NOTFOUNDS + 1));        
+				let REPO_NOTFOUNDS++
 			fi
-		done	
-
-		# mostrando proxima verificacao
-		# echo "Proxima verificação em:" >> /tmp/repo.txt
-		# date -d "$TEMPO_DATE" >> /tmp/repo.txt
-
-		# aguardando tempo especifico
-	# 	sleep $TEMPO
-	# done
+		fi
+	done	
 }
 
 # data de inicio do script
 echo "Inicio do script" > /tmp/repo.txt
 date > /tmp/repo.txt
 printf "\n" >> /tmp/repo.txt
-
-# enviando saida para main.txt
-# printf "\n\nArquivo repo.sh executado" >> /tmp/main.txt
 
 # chamando funcao
 internet
