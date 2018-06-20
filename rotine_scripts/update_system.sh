96fa51e4c4a8b133092d116fcc8f1dbd3e8f362a
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-06-20 13:09:04 -0300

    arquivo adicionado - atualiza sistema

---
+++ rotine_scripts/update_system.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+# 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+ 
+#                           CABEÇALHO DO SCRIPT                               #
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# 
+# # # # # # # # # # # # # 
+#   FONTES DE PESQUISA  #
+# # # # # # # # # # # # # 
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+# # # # # # # # # # # # # 
+#   DESENVOLVIDO POR    #
+# # # # # # # # # # # # # 
+#
+# por lenonr(Lenon Ricardo) 
+#       contato: <lenonrmsouza@gmail.com>
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#										#
+#	If I have seen further it is by standing on the shoulders of Giants.	#
+#	(Se vi mais longe foi por estar de pé sobre ombros de gigantes)		#
+#							~Isaac Newton		#
+#										#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # versão do script:           [0.0.4.0.0.0]   #
+# # data de criação do script:    [26/10/17]      #
+# # ultima ediçao realizada:      [26/10/17]      #
+# # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+# Legenda: a.b.c.d.e.f
+# 	a = alpha[0], beta[1], stable[2], freeze[3];
+# 	b = erros na execução;	
+# 	c = interações com o script;
+# 	d = correções necessárias;
+# 	e = pendencias    
+# 	f = desenvolver 
+# 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# 
+#       [+] - Açao realizada 
+#       [*] - Processamento
+#       [-] - Não executado
+# 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# 
+# # Script testado em
+#       - Debian 9
+#
+# # Compativel com
+#       - Debian 9
+# 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# 
+# # FUNCOES
+# 
+# # # # # ATUALIZA SISTEMA
+# # [+] Update
+# # [+] Upgrade
+# 
+# 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+ 
+#                               CORPO DO SCRIPT                               #
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# 
+# 
+# # ATUALIZA SISTEMA
+# 
+    update()
+    {    
+	clear
+
+        #atualizando lista de repositorios            
+        printf "[+] Atualizando lista de repositorios do sistema \n"   
+        
+        apt update         
+    }
+    
+    upgrade()
+    {            
+	clear
+
+        #atualizando lista de programas do sistema
+        printf "[+] Atualizando lista de programas do sistema \n"  
+        
+        apt upgrade -y 
+    }
+# 
+# # # # # # # # # #
+# 
+##REALIZANDO VERIFICAÇÕES
+    ######VERIFICANDO USUARIO ROOT
+    if [[ `id -u` -ne 0 ]]; then
+        clear
+        printf "[-] Você precisa ter poderes administrativos (root) \n"
+        printf "[-] O script está sendo finalizado... \n"
+        sleep 3
+        exit
+    fi
+# 
+#mostrando mensagem inicial
+    menu()
+    {    
+        clear
+        
+        # executando menu
+        update
+        upgrade
+    }
+    
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# INICIANDO SCRIPT
+menu
+# 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+ 
+#                           RODAPE DO SCRIPT                                    #
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
