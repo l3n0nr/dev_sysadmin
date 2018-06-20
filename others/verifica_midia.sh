9496cf6c46d59576996c5c5a9fc4cf8829f159d8
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-06-20 00:15:12 -0300

    arquivo melhorado

--- others/verifica_midia.sh
+++ others/verifica_midia.sh
@@ -8,11 +8,11 @@
 # 	contato: <lenonrmsouza@gmail.com>
 #
 #################################################################################
-#										#
-#	If I have seen further it is by standing on the shoulders of Giants.	#
-#	(Se vi mais longe foi por estar de pé sobre ombros de gigantes)		#
-#							~Isaac Newton		#
-#										#
+#										                                        #
+#	If I have seen further it is by standing on the shoulders of Giants.	    #
+#	(Se vi mais longe foi por estar de pé sobre ombros de gigantes)		        #
+#							~Isaac Newton		                                #
+#										                                        #
 #################################################################################
 #
 ###################################################
