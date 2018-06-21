f605b6c0dd5b3d5fc6d962953fe646d5c539f25c
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-06-20 23:29:08 -0300

    funcao check_git adicionada

--- rotine_scripts/arquivos_git/status_git.sh
+++ rotine_scripts/arquivos_git/status_git.sh
@@ -86,4 +86,48 @@ status_git()
 	date >> /tmp/repo.txt
 }
 
+check_git()
+{
+	# clear
+
+	# # walk to the array
+	for (( i = 0; i <= ${#repos[@]}; i++ )); do	
+		# verify local repo disk
+		if [[ $local${repos[$i]} != $local ]]; then
+			# verify local repo
+			if [ -e "$local${repos[$i]}" ]; then 	  	 
+				# if update repositorie work
+			  	if [[ $? == "0" ]]; then
+		  			# into folder location
+				  	cd $local${repos[$i]}			  					
+
+					# check status
+					git status | grep "to publish your local commits" > /dev/null
+  											
+					# if value = 0, then comparation is true
+					if [[ $? == "0" ]]; then
+						zenity --notification \
+							   --text "Ei $USER, voce precisa dar push em ${repos[$i]}!"
+					fi														
+				# if update repositorie not work		
+			  	else				  		
+					echo "repositorie Error ${repos[$i]}!" >> /tmp/repo.txt
+			  	fi				
+				
+				# add  
+			  	let repo_founds++		
+
+			  	printf "\n" >> /tmp/repo.txt  	
+			else
+				date >> /tmp/repo.txt
+				echo "[-] - Not found": $local${repos[$i]}
+
+				# repo_notfounds=$(($repo_notfounds + 1));        
+				let repo_notfounds++
+			fi
+		fi		
+	done
+}
+
 status_git
+check_git
