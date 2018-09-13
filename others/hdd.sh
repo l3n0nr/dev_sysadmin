f882ae497337ebaa8b77fe20ab6e8c916729a928
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-09-13 15:19:22 -0300

    variaveis otimizadas

--- others/hdd.sh
+++ others/hdd.sh
@@ -2,22 +2,22 @@
 
 # Descricao: Chama script de backup caso caminho exista, via alias.
 
+hdd="/media/lenonr/BACKUP"
+local_hdd='$hdd/Arquivos/Filmes'
+
 verificahd()
 {	
-    local_hdd='/media/lenonr/BACKUP/Arquivos/Filmes'
-
-  	clear 
-
   	# verificando se diretorio existe 
   	if [ -e "$local_hdd" ]; then 
       /home/lenonr/Github/dev_sysadmin/others/verifica_midia.sh
   	else
-		echo "Conecte o HDD ao computador!"
+		echo "Conecte o $hdd ao computador!"
 	fi
 }
 
 main()
 {
+	clear
 	verificahd
 }
 
