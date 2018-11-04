f34cc156f6b152a411ee86e8b036ffccd6a79a30
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-11-03 23:47:08 -0300

    if melhorado

--- others/sistema.sh
+++ others/sistema.sh
@@ -22,7 +22,7 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# # versão do script:              [0.0.35]       #
+# # versão do script:              [0.0.40]       #
 # # data de criação do script:    [23/10/17]      #
 # # ultima ediçao realizada:      [03/11/18]      #
 # # # # # # # # # # # # # # # # # # # # # # # # # # 
@@ -70,22 +70,35 @@ instalacao_sistema()
 
 completo()
 {
-	echo "###############################################################################"
 	neofetch_sistema
 	memoria_utilizada
 	disco
 	instalacao_sistema	
-	echo "###############################################################################"
 }
 
 main()
 {	
-	if [[ $sistema == "notebook" ]]; then
-		echo "###############################################################################"
+	echo "###############################################################################"
+	if [[ $1 == "" ]]; then
+		echo "Parametros disponiveis:"
+		echo "-n: neofetch"
+		echo "-d: disco"
+		echo "-m: memoria utilizada"	
+		echo "-a: completo"
+	elif [[ $1 == "-a" ]]; then
+		completo
+	elif [[ $1 == "-n" ]]; then
+		neofetch_sistema
+	elif [[ $1 == "-d" ]]; then
+		disco
+	elif [[ $1 == "-m" ]]; then
+		memoria_utilizada
+	elif [[ $sistema == "notebook" ]]; then		
 		neofetch_sistema
 	else
 		completo
 	fi
+	echo "###############################################################################"
 }
     
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
