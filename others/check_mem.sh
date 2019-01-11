669d0cbb56f3bca93b3f233eb2d6a39ca82f949e
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-11 15:13:44 -0200

    arquivo melhoradao

--- others/check_mem.sh
+++ others/check_mem.sh
@@ -3,11 +3,11 @@
 ##########################
 ## DESCRICAO DO SCRIPT
 #	Verifica memoria total e envia mensagem para usuario
-##########################
 #
 # DATA_CRIACAO: 09/01/19
-# ULT_MODIFIC:  09/01/19
+# ULT_MODIFIC:  11/01/19
 # AUTOR:		lenonr
+###########################
 #
 # variaveis
 mem_liv=$(($(free -t | awk '/Total:/ {print $4}') / 1024))
@@ -15,13 +15,10 @@ mem_lim="500"
 #
 verifica()
 {
-	# echo $mem_liv "MB"
-	# echo $mem_lim "MB"
-
 	if [[ $mem_liv > $mem_lim ]]; then
 		notify-send "Aparentemente esta lento, $USER?"
 	else
-		notify-send "Super-rapidao ne, $USER!"
+		notify-send "Normal, $USER!"
 	fi
 }
 
