32bff79348c0f8dba405498d947656a87392086f
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-12 16:29:44 -0200

    arquivo melhorado

--- others/check_mem.sh
+++ others/check_mem.sh
@@ -5,29 +5,27 @@
 #	Verifica memoria total e envia mensagem para usuario
 #
 # DATA_CRIACAO: 09/01/19
-# ULT_MODIFIC:  11/01/19
-# VERSAO: 		0.45
+# ULT_MODIFIC:  12/01/19
+# VERSAO: 		0.46
 # AUTOR:		lenonr
 ###########################
 #
 export DISPLAY=":0.0"
 
 # variaveis
-mem_total="$(($(free -t | awk '/Total:/ {print $2}') / 1024))"
 mem_liv="$(($(free -t | awk '/Total:/ {print $4}') / 1024))"
-
-# taxa="30"
-
-# mem_taxa="$(echo $mem_total/$mem_liv | bc)"
-
+mem_war="1500"
 mem_lim="1000"
-#
+
 verifica()
 {
-	if [[ "$mem_lim" -ge "$mem_liv" ]]; then
+	if [[ "$mem_liv" -le "$mem_lim" ]]; then
 		notify-send -t 10000 "Aparentemente esta lento, apenas $mem_liv MB livre!"
-	# else
-	# 	notify-send "Normal, $mem_liv MB $USER!"
+	elif [[ "$mem_war" -ge "$mem_liv" ]]; then
+		notify-send -t 10000 "Memoria em atençao $mem_liv MB!"
+	else
+		# notify-send -t 10000 "Memoria normal!"
+		echo ""
 	fi
 }
 
