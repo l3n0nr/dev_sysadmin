ef139e096818dd4c539aa6875320239a103efe72
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-27 10:26:23 -0200

    profile improviment

---
+++ others/radio/main.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+##########################################
+# REFERENCIA 
+#		https://youtu.be/8Lam1iVZIGA
+#
+# DESCRICAO
+#		Toca web radio no terminal
+##########################################
+#
+# DATA_CRIACAO: 26/01/19
+# ULT_MODIFIC:  27/01/19
+#
+verifica_internet()
+{
+	ping_server="www.google.com"
+	
+  	ping -c1 $ping_server >> /dev/null
+  	[[ ! $? -eq 0 ]] && echo "SEM CONEXAO!" && exit 1
+}
+
+radio()
+{
+	declare -A STREAM
+	
+	source radio
+
+	PS3=$'\nSelecione uma radio:'
+	select radio in "${!STREAM[@]}"; do
+		title="${radio}"
+		ip="${STREAM[$radio]}"
+		echo -e "\033[31;1mVoce esta ouvindo: $title (Ctrl+C para sair)\033[m"
+		ffplay -nodisp $ip &> /dev/null
+	done
+}
+
+main()
+{
+	clear
+	# verifica_internet
+	radio
+}
+
+main
