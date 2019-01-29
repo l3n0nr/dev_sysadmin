1366544fbdb65a239784ca1535a6657556b14f8b
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-29 16:29:35 -0200

    tentando fazer interface em dialog, nao sei se vai rolar nao :/

--- others/radio/main.sh
+++ others/radio/main.sh
@@ -9,12 +9,14 @@
 ##########################################
 #
 # DATA_CRIACAO: 26/01/19
-# ULT_MODIFIC:  27/01/19
-# VERSAO:		0.35
+# ULT_MODIFIC:  29/01/19
+# VERSAO:		0.38
 #
 verifica_internet()
 {
 	echo "Verificando conexao, aguarde..."
+	clear 
+
 	ping_server="www.google.com"
 	
   	ping -c1 $ping_server >> /dev/null
@@ -38,11 +40,40 @@ radio()
 	done
 }
 
+radio_dialog()
+{
+
+	declare -A STREAM
+	
+	local="/home/lenonr/Github/dev_sysadmin/others/radio"
+
+	source $local/radio.conf
+	
+	# for (( i = 0; i <= ${!STREAM[@]}; i++ )); do  
+	# 	name = "${i}"
+	# 	ip   = "${STREAM[$i]}"
+	# done
+
+	# select radio in "${!STREAM[@]}"; do
+		# title="${radio}"
+		# ip="${STREAM[$radio]}"
+		# echo -e "\033[31;1mVoce esta ouvindo: $title (Ctrl+C para sair)\033[m"
+		# ffplay -nodisp $ip &> /dev/null\
+
+		# PS3=$(dialog \
+	 #            --stdout --ok-label "Ouvir" --cancel-label "Cancelar" \
+	 #            --menu "Escolha uma radio:" \
+	 #            0 0 0 \
+	 #            "${radio}" "${STREAM[$radio]}")
+	# done	
+}
+
 main()
 {
 	clear
 	verifica_internet
 	radio
+	# radio_dialog
 }
 
 main
