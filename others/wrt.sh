15988c5625fe346a37ddff063e10124157832ddd
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-01-30 15:27:24 -0200

    arquivo melhorado, funcoes organizadas

--- others/wrt.sh
+++ others/wrt.sh
@@ -1,29 +1,32 @@
 #!/usr/bin/env bash
 
-# Descricao: Chama telnet do roteador via alias
+# DESCRICAO
+#   Chama telnet do roteador via alias
+
+local_wrt='10.0.0.87'    
+
+# ULT_MOD = 30/01/19
 
 wrt()
-{
-    local_wrt='10.0.0.87'
-
-  	# verificando conexao
-  	ping -c1 $local_wrt
-
-  	clear 
+{    
+    # verificando conexao
+    ping -c1 $local_wrt >> /dev/null  
 
   	# verificando se diretorio existe 
   	if [ $? -eq 0 ]; then
-  		echo "########################"
-  		printf "CONECTANDO AO SERVIDOR\n"
-  		echo "########################\n"
+  		printf "########################"
+  		printf "\nCONECTANDO AO SERVIDOR\n"
+  		printf "########################\n"
 	  	telnet $local_wrt
   	else
-		echo "IP do servidor '$local_wrt' nao encontrado"
+		  echo "IP do servidor '$local_wrt' nao encontrado"
 	fi
 }
 
 main()
 {
+  clear
+
   wrt
 }
 
