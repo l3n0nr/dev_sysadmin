0d7abf2bb063310846d412778144df1d1b0faa1a
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-02-04 22:05:07 -0200

    porcentagem swap melhorada

--- others/reboot_swap.sh
+++ others/reboot_swap.sh
@@ -41,7 +41,7 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # # versão do script:           [0.1.104.0.0.0]    #
 # # data de criação do script:    [03/11/17]      #
-# # ultima ediçao realizada:      [03/02/19]      #
+# # ultima ediçao realizada:      [04/02/19]      #
 # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # 
 # Legenda: a.b.c.d.e.f
@@ -97,7 +97,7 @@ limpa()
 porcentagem()
 {
 	# minimo de memoria RAM extra para ser realizada a limpeza(porcentagem)
-	porcentagem_mem="46"
+	porcentagem_mem="48"
 
 	# variaveis de verificacao da memoria RAM
 	memoria_total=$(free | awk '/Mem:/ {print $2}')	
