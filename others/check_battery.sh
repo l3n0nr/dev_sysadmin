39a7119025080889dd3aec03efe45349746f079d
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-03-07 16:41:27 -0300

    verificar expected shutdown

--- others/check_battery.sh
+++ others/check_battery.sh
@@ -7,7 +7,7 @@
 #
 # DAT_CRIAC	:	07/01/19
 # LAST_MOD	:	07/03/19
-# VERSAO	:	0.75
+# VERSAO	:	0.80
 # AUTOR 	:	lenonr
 #
 ########################
@@ -17,7 +17,6 @@
 #
 # VARIAVEIS
 aguarda="1"
-lista=('[.  ]' '[.. ]' '[...]')	
 
 check()
 {
@@ -29,6 +28,15 @@ check()
 
 	time="$(ibam --percentbattery | grep "Battery time left:"| awk {'print $4'})"
 	percent="$(ibam --percentbattery | grep "Battery percentage:"| awk {'print $3$4'})"
+	level="$(ibam --percentbattery | grep "Battery percentage:"| awk {'print $3'})"
+	# expected_time="$(date -d $time +%Hh%Mm%Ss)"
+
+	expected_time_h=${time:0:1}
+	expected_time_m=${time:2:2}
+	expected_time_s=${time:4:4}
+
+	# expected_time="$(date -d $expected_time_m $expected_time_s +%X)"
+	expected_time="????"
 
 	if [[ $current > 0 ]]; then
 		current_now="$(($current / 1000))"	
@@ -46,30 +54,59 @@ check()
 	date_rest="$battery_res"
 
 	if [[ $low_res -ge $current_now ]] ; then
-		consuming_level="[+++------]"
+		consuming_level="[+++-------]"
 	elif [[ $med_res -ge $current_now ]] ; then
-		consuming_level="[++++++---]"
+		consuming_level="[+++++++---]"
 	elif [[ $high_res -ge $current_now ]] ; then
-		consuming_level="[+++++++++]"
+		consuming_level="[++++++++++]"
 	else
 		consuming_level="[**ERROR**]"
-	fi
+	fi	
 
-	percent_level="[?????????]"
+	if [[ "10" -ge $level ]]; then
+		percent_level="[+---------]"
+	elif [[ "20" -ge $level ]]; then
+		percent_level="[++--------]"
+	elif [[ "30" -ge $level ]]; then
+		percent_level="[+++-------]"
+	elif [[ "40" -ge $level ]]; then
+		percent_level="[++++------]"
+	elif [[ "50" -ge $level ]]; then
+		percent_level="[+++++-----]"
+	elif [[ "60" -ge $level ]]; then
+		percent_level="[++++++----]"
+	elif [[ "70" -ge $level ]]; then
+		percent_level="[+++++++---]"
+	elif [[ "80" -ge $level ]]; then
+		percent_level="[++++++++--]"
+	elif [[ "90" -ge $level ]]; then
+		percent_level="[+++++++++-]"
+	elif [[ "100" -ge $level ]]; then
+		percent_level="[++++++++++]"
+	else
+		percent_level="[**ERROR**]"
+	fi	
 
 	if [[ $status == "Discharging" ]]; then						
-		echo "Status battery:" $status	
+		# echo "Status battery:" $status	
+
+		echo -e "Status battery:\e[1;31m $status"" \e[0m"
 		echo "Time rest:" $time "/" $percent	
-		echo "Consuming now:" $current_now "mA /" $consuming_level
+		echo "Consuming now:" $current_now "mA"
 		echo "Baterry rest:" $charge_now "mAh / $percent_level"		
+		echo "Expected shutdown:" $expected_time
 	elif [[ $status == "Charging" ]]; then						
-		echo "Status battery:" $status	
+		# echo "Status battery:" $status
+
+		echo -e "Status battery:\e[1;32m $status"" \e[0m"
 		echo "Percent to full:" $(((100 - $perc_batery))) "%"
 		echo "Current battery now:" $current_now "mA"		
 		echo "Consuming level energy:" $consuming_level
 		echo "		$battery_full | $current_now"
 	elif [[ $status == "Full" ]]; then
-		echo "Status battery: Full"
+		# echo "Status battery: Full"
+
+		echo -e "Status battery:\e[1;34m Full"" \e[0m"
 	else
 		echo "ERROR"
 	fi		
@@ -86,15 +123,11 @@ check()
 main()
 {	
 	while [[ TRUE ]]; do		
-		for (( i = 0; i <= ${#lista[@]}-1; i++ )); do  
-			clear
-			echo "###################################"
-			check
-			echo "###################################"
-			sleep 1
-			# printf "${lista[$i]}"
-			# sleep 0.3
-		done		
+		clear
+		echo "###################################"
+		check
+		echo "###################################"
+		sleep 3
 	done	
 }
 
