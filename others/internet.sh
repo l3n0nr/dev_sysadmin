404e0aa65df43adaded67d90d6fb04ca77a4e47f
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-05-02 11:41:04 -0300

    arquivo melhorado

---
+++ others/internet.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#
+# # # # # # # # # # # # #
+#   DESENVOLVIDO POR    #
+# # # # # # # # # # # # #
+#
+# por lenonr(Lenon Ricardo)
+#       contato: <lenonrmsouza@gmail.com>
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # #
+# # data de criação do script:    [28/03/18]      #
+# # ultima ediçao realizada:      [02/05/19]      #
+# # versao do script :			  [	 0.20  ] 	  #
+# # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+#
+## VARIAVEIS
+servidor="google.com"
+tempo=60
+
+check_internet()
+{
+	data=$(date +%k:%M:%S)
+	ping -q -c4 $servidor &> /dev/null 
+
+	if [ $? == "0" ]; then	
+		mensagem="Internet funcionando!"
+		echo $mensagem $data
+		notify-send "$mensagem"
+		exit 1
+	else				
+		mensagem="- Sem conexao!"		
+		echo $mensagem $data
+	fi		
+}
+
+while_internet()
+{
+	while true; do			
+		check_internet
+		sleep $tempo
+	done
+}
+
+main()
+{
+	clear
+	while_internet
+}
+
+main
