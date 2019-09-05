d0c88f90a0c63b8eb19dab4d550464fe47f5deed
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-09-05 18:31:50 -0300

    wmctrl add

--- others/close_window.sh
+++ others/close_window.sh
@@ -4,20 +4,22 @@
 #
 # DESCRICAO: Fecha janela de acordo com o PID
 # DATA_CRIA: 04/09/19
-# ULT_MODIF: 04/09/19
-# VERSAO:	 0.10
+# ULT_MODIF: 05/09/19
+# VERSAO:	 0.15
+#
+# REFERENCE:
+# 	<https://askubuntu.com/questions/553203/how-to-close-only-one-window-of-an-application>
 #
 ####################
 #
 get_pid()
 {
-	window_pid=$(xdotool getactivewindow getwindowpid)
-
-	kill $window_pid
+	wmctrl -c :ACTIVE:
 }
 
 main()
 {
+	sleep 2
 	get_pid
 }
 
