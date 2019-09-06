fd3a88f60cab2d90472096bc4cdf5685c0bd9390
Lenon Ricardo <lenonrmsouza@gmail.com>
2019-09-05 22:33:27 -0300

    funcoes separados e chamadas via parametro

--- others/close_window.sh
+++ others/close_window.sh
@@ -5,26 +5,45 @@
 # DESCRICAO: Fecha janela de acordo com o PID
 # DATA_CRIA: 04/09/19
 # ULT_MODIF: 05/09/19
-# VERSAO:	 0.15
+# VERSAO:	 0.20
 #
 # REFERENCE:
 # 	<https://askubuntu.com/questions/553203/how-to-close-only-one-window-of-an-application>
 #
 ####################
 #
-get_pid()
+close_window()
 {
+	## descobre nome da janela ativa
 	window_name=$(xdotool getactivewindow getwindowname)
 
-	# se estiver na area de trabalho nao executa 
-	if [[ $window_name != "Área de trabalho" ]]; then
+	## se estiver na area de trabalho volta para a janela anterior
+	## senao fecha a atual.
+	if [[ $window_name != "Área de trabalho" ]]; then		
 		wmctrl -c :ACTIVE:		
+	fi
+}
+
+show_window()
+{
+	## descobre nome da janela ativa
+	window_name=$(xdotool getactivewindow getwindowname)
+
+	if [[ $window_name == "Área de trabalho" ]]; then
+		wmctrl -k off		
+	else
+		wmctrl -k on
 	fi
 }
 
 main()
 {
-	get_pid
+	if [[ $1 == "close" ]]; then
+		close_window
+	else
+		show_window
+	fi
+
 }
 
-main
+main $1
