87228430f8671da850c3dcd022ca7f5d7c2226ec
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-10-09 20:49:32 -0300

    arquivo melhroado

--- others/battery/check_temp.sh
+++ others/battery/check_temp.sh
@@ -2,27 +2,59 @@
 #
 ######################################################################
 # DESCRICAO
-#		Extrai temperatura do processador
+#		Extrai temperatura do processador 
+#		e desliga o computador, caso necessario.
 ######################################################################
 #
 # DAT_CRIAC	:	08/10/20
-# LAST_MOD	:	08/10/20
-# VERSAO	:	0.15
+# LAST_MOD	:	09/10/20
+# VERSAO	:	0.20
 # AUTOR 	:	l3n0nr
 ######################################################################
 #
-temperature()
-{
-	temp=$(tlp-stat -t | grep CPU | awk {'print $4'})
-	margem="85"
+# capturando temperatura
+temp=$(tlp-stat -t | grep CPU | awk {'print $4'})
 
+# valores de referencia
+margem_lim="95"
+margem="85"
+
+# mensagens para o usuario
+mensagem="CPU à $temp C, é melhor desligar."
+mensagem_limite="Computador desligando AGORA!"
+
+# saida de log
+log_temp="/tmp/log_temp"
+
+# data personalizada
+tempo_verifica=$(date)
+
+check_files()
+{
+	if [[ ! -e $log_temp ]]; then
+		touch $log_temp
+		echo "" > $log_temp
+	fi
+}
+
+temperature()
+{	
 	if [[ $temp > $margem ]]; then
-		notify-send "Computador quente, é melhor desliga-lo"
+		notify-send -t 10000 "$mensagem"
+	fi
+
+	if [[ $temp > $margem_lim ]]; then
+		notify-send -t 10000 "$mensagem_limite"
 	fi		
+	
+	echo "" $tempo_verifica >> $log_temp
 }
 
 main()
 {
+	clear
+
+	check_files
 	temperature
 }
 
