e79d4041236aa5f7e61a7f6fa9e22e6e4511c6cc
Lenon Ricardo <lenonrmsouza@gmail.com>
2020-10-11 19:27:02 -0300

    arquivo melhorado - main

--- others/ip_externo.sh
+++ others/ip_externo.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/env bash
 
-curl icanhazip.com
+func_verifica_internet()
+{
+	ping_server="google.com"
+
+	# verificando internet
+  	# se nao tiver internet, fecha script
+  	ping -c1 $ping_server >> /dev/null
+
+  	if [[ $? -eq 0 ]]; then
+  		clear
+  		printf "IP:" && curl icanhazip.com
+  	else
+  		clear
+  		echo "Sem conexao"
+  	fi
+}
+
+main()
+{
+	func_verifica_internet
+}
+
+main
+
